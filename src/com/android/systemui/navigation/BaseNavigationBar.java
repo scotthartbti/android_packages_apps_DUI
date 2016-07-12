60243bd1f1ff43c2f55d33ea7cd20e884f2e4820
Randall Rushing <randall.rushing@gmail.com>
2016-07-12 15:13:41 +0200

    Pulse 2.0 Advanced Audio/Visual Rendering [1/3]
    Introduce a completely overhauled Pulse framework and bring
    in a brand new renderer based on the CyanogenMod lockscreen
    visualizer. This new renderer is very robust and opens
    many more rendering doors. Select between solid lines, fading blocks,
    and Off.
    
    Change-Id: I4d01767298316653644f2907f9e68f6c840064f8

--- src/com/android/systemui/navigation/BaseNavigationBar.java
+++ src/com/android/systemui/navigation/BaseNavigationBar.java
@@ -386,7 +386,7 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
     public final void dispose() {
         mSmartObserver.cleanUp();
         if (mPulse != null) {
-            mPulse.removePulseObserver();
+            mPulse.doUnlinkVisualizer();
         }
         flushSpringSystem();
         onDispose();
@@ -582,8 +582,10 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
             reorient();
             notifyVerticalChangedListener(newVertical);
         }
-
         postCheckForInvalidLayout("sizeChanged");
+        if (mPulse != null) {
+            mPulse.onSizeChanged(w, h, oldw, oldh);
+        }
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
