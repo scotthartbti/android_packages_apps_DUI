1c09c62af881b29990fc6b649bbf0c5bf3e0c76d
Varun Date <date.varun123@gmail.com>
2016-07-22 20:01:31 +0200

    DUI:Pulse: Add More Dimens To Fading Blocks [2/2]
    Now Spacing Is much more dyanamic and gives us the opportunity to increase number of bars
    
    Screenshot : https://s31.postimg.org/r4n2d13rf/screener_20160722_11_51_03.png
    
    AICPfy:
    Fix up
    config_pulseDivisions typos
    
    Change-Id: I9cc35dfd9b4e485f4c1c20f6e076748dfba77a23
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
+++ src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
@@ -253,7 +253,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                     resolver, Settings.Secure.PULSE_CUSTOM_DIMEN, 14,
                     UserHandle.USER_CURRENT);
             int numDivision = Settings.Secure.getIntForUser(
-                    resolver, Settings.Secure.PULSE_CUSTOM_DIV, 2,
+                    resolver, Settings.Secure.PULSE_CUSTOM_DIV, 8,
                     UserHandle.USER_CURRENT);
             int fudgeFactor = Settings.Secure.getIntForUser(
                     resolver, Settings.Secure.PULSE_CUSTOM_FUDGE_FACTOR, 2,
@@ -430,32 +430,29 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                 mPaint.setStrokeWidth(mContext.getResources().getDimensionPixelSize(
                         R.dimen.config_pulsePathStrokeWidth29));
             }
-            if (numDivision == 0) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions);
-            }
-            else if (numDivision == 1) {
+            if (numDivision == 1) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions1);
             }
             else if (numDivision == 2) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions);
-            }
-            else if (numDivision == 3) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions2);
             }
-            else if (numDivision == 4) {
+            else if (numDivision == 3) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions3);
             }
-            else if (numDivision == 5) {
+            else if (numDivision == 4) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions4);
             }
-            else if (numDivision == 6) {
+            else if (numDivision == 5) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions5);
             }
-            else if (numDivision == 7) {
+            else if (numDivision == 6) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions6);
             }
-            else if (numDivision == 8) {
+            else if (numDivision == 7) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions7);
+            }
+            else if (numDivision == 8) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions);
             }
             else if (numDivision == 9) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions8);
@@ -467,22 +464,40 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions10);
             }
             else if (numDivision == 12) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions11);
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions10);
             }
             else if (numDivision == 13) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions12);
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions11);
             }
             else if (numDivision == 14) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions13);
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions12);
             }
             else if (numDivision == 15) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions14);
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions13);
             }
             else if (numDivision == 16) {
-                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions15);
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions14);
             }
             else if (numDivision == 17) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions15);
+            }
+            else if (numDivision == 18) {
                 mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions16);
+            }
+            else if (numDivision == 19) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions17);
+            }
+            else if (numDivision == 20) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions18);
+            }
+            else if (numDivision == 21) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions19);
+            }
+            else if (numDivision == 22) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions20);
+            }
+            else if (numDivision == 23) {
+                mDivisions = mContext.getResources().getInteger(R.integer.config_pulseDivisions21);
             }
             if (fudgeFactor == 0) {
                 mDbFuzzFactor = mContext.getResources().getInteger(
