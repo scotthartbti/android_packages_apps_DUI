5c599b8482336c7a7f982ca6b6912a511a1843cc
Ali B <abittin@gmail.com>
2017-01-24 18:46:09 +0300

    DUI: add missing import
    Change-Id: I022ed90c0d6e1d0c34a3ab5334d7773bfbd9abb4

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -36,6 +36,7 @@ import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
