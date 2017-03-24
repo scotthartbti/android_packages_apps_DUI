8cfd89af06e4211ca841b68fd7353a315df2cb10
Varun Date <date.varun123@gmail.com>
2017-03-24 18:52:39 +0100

    DUI: SmartBar Button Tint [2/3]
    With Reference to Patch https://github.com/AICP/frameworks_base/commit/85d97f4ad3b4d8933f48d9f2c09da049c79a409a
    
    thanks to lichti1901 for this initial Patch
    
    Patchset 2 : Fix Back Button Tinting
    Thanks to  @LorDClockaN
    
    Based on this patch
    
    https://github.com/AICP/frameworks_base/commit/d09e1cc1295e2f21db8e7908f650e26f0d6bb2b6
    
    Patchset 3 : Fix New Actions FCs
    
    Change-Id: I545001fb4a21290fa8848331377f7e64ec0f33b5
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -32,6 +32,7 @@ import android.app.StatusBarManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -134,6 +135,8 @@ public class SmartBarView extends BaseNavigationBar {
     private int mImeHintMode;
     private int mButtonAnimationStyle;
     private float mCustomAlpha;
+    private static boolean mNavTintSwitch;
+    public static int mIcontint;
 
     private GestureDetector mNavDoubleTapToSleep;
     private SlideTouchEvent mSlideTouchEvent;
@@ -235,6 +238,10 @@ public class SmartBarView extends BaseNavigationBar {
 
     public void setButtonDrawable(SmartButtonView button) {
         ButtonConfig config = button.getButtonConfig();
+        mNavTintSwitch = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.NAVBAR_TINT_SWITCH, 0) == 1;
+        mIcontint = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.NAVBAR_BUTTON_COLOR, 0xFFFFFFFF);
         Drawable d = null;
         if (config != null) {
             // a system navigation action icon is showing, get it locally
@@ -255,6 +262,20 @@ public class SmartBarView extends BaseNavigationBar {
                 button.setImageDrawable(null);
                 button.setImageDrawable(d);
             }
+            if (mNavTintSwitch) {
+            button.setColorFilter(mIcontint, Mode.SRC_IN);
+            } else {
+            button.setColorFilter(null);
+            }
+        }
+    }
+
+    public static int updatetint() {
+        if (mNavTintSwitch) {
+            return mIcontint;
+        } else {
+            mIcontint = -1 ;
+            return mIcontint;
         }
     }
 
