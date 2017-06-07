41aa41ef5752629b6a05018ca8ecee8769e818bb
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-06-07 00:38:05 -0700

    Pulse: Navbar left in landscape support
    By default now, AOSP places the navigation bar on the left side
    of the screen on phones when rotated 270 degrees. Adapt for this
    change by updating Pulse when orientation changes.
    
    The orientation listener will be enabled disabled according to the screen
    state (screen on, screen off, keyguard showing or not, keyguard bypassed after
    fingerprint unlock thus screen on straight).
    
    Change-Id: I3f77ec134f25e2d11cdde40037c81a875af5e143

--- src/com/android/systemui/navigation/BaseNavigationBar.java
+++ src/com/android/systemui/navigation/BaseNavigationBar.java
@@ -53,11 +53,13 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Display;
+import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
@@ -90,7 +92,9 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
     protected View mCurrentView = null;
     protected FrameLayout mRot0, mRot90;
     protected int mDisabledFlags = 0;
+    private int mLastRotation = Surface.ROTATION_0;
     protected int mNavigationIconHints = 0;
+    private boolean isOrientationListenerEnabled;
     protected boolean mVertical;
     protected boolean mScreenOn;
     protected boolean mLeftInLandscape;
@@ -98,6 +102,7 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
     protected boolean mWakeAndUnlocking;
     protected boolean mScreenPinningEnabled;
     protected OnVerticalChangedListener mOnVerticalChangedListener;
+    protected OrientationEventListener mOrientationEventListener;
     protected SmartObserver mSmartObserver;
     protected PulseController mPulse;
     protected PhoneStatusBar mBar;
@@ -155,6 +160,21 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
         mSmartObserver = new SmartObserver(mHandler, context.getContentResolver());
         mSpringSystem = SpringSystem.create();
         sIsTablet = !DUActionUtils.navigationBarCanMove();
+        mOrientationEventListener = new OrientationEventListener(mContext,
+                SensorManager.SENSOR_DELAY_NORMAL) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                int rotation = mDisplay.getRotation();
+                if (rotation != mLastRotation) {
+                    setLeftInLandscape(rotation == Surface.ROTATION_270);
+                }
+                mLastRotation = rotation;
+            }
+        };
+        if (mOrientationEventListener.canDetectOrientation()) {
+            mOrientationEventListener.enable();
+            isOrientationListenerEnabled = true;
+        }
     }
 
     // require implementation. Surely they have something to clean up
@@ -311,6 +331,14 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
             if (mPulse != null) {
                 mPulse.setKeyguardShowing(showing);
             }
+            if (mScreenOn && !mKeyguardShowing && !isOrientationListenerEnabled
+                    && mOrientationEventListener.canDetectOrientation()) {
+                mOrientationEventListener.enable();
+                isOrientationListenerEnabled = true;
+            } else if (isOrientationListenerEnabled) {
+                mOrientationEventListener.disable();
+                isOrientationListenerEnabled = false;
+            }
             onKeyguardShowing(showing);
         }
     }
@@ -395,6 +423,14 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
         mScreenOn = screenOn;
         if (mPulse != null) {
             mPulse.notifyScreenOn(screenOn);
+        }
+        if (mScreenOn && !mKeyguardShowing && !isOrientationListenerEnabled
+                && mOrientationEventListener.canDetectOrientation()) {
+            mOrientationEventListener.enable();
+            isOrientationListenerEnabled = true;
+        } else if (isOrientationListenerEnabled) {
+            mOrientationEventListener.disable();
+            isOrientationListenerEnabled = false;
         }
         setDisabledFlags(mDisabledFlags, true);
     }
